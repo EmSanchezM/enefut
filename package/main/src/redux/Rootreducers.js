deae0ea868d92f6f32ceb0f7b294f76b712471ee
Elvin Sanchez <emsanchez891@gmail.com>
2022-01-09 09:45:54 -0600

    correction of activities

--- package/main/src/redux/Rootreducers.js
+++ package/main/src/redux/Rootreducers.js
@@ -3,12 +3,14 @@ import CustomizerReducer from './customizer/CustomizerReducer';
 import chatReducer from './chats/ChatReducer';
 import notesReducer from './notes/NotesReducer';
 import emailReducer from './email/EmailReducer';
+import activityReducer from './activities/ActivityReducer';
 
 const RootReducers = combineReducers({
   CustomizerReducer,
   chatReducer,
   notesReducer,
   emailReducer,
+  activityReducer,
 });
 
 export default RootReducers;
